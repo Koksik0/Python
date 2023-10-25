9f11ed818d37eb8afaf4bee6925795440ffbe430
Paweł Wacławik <pawelwoe@gmail.com>
2023-10-25 12:57:41 +0200

    Zadanie 3.2

--- Zestaw3/Zadanie2.py
+++ Zestaw3/Zadanie2.py
@@ -1,11 +1,11 @@
-L = [3, 5, 4] ; L = L.sort()
+L = [3, 5, 4] ; L = L.sort() #Po wykonaniu tych operacjacji do L zostanie przypisane None, ponieważ sort() zmienia listę na miejscu
 
-x, y = 1, 2, 3
+x, y = 1, 2, 3 #Błąd ponieważ prubujemy przypisać 3 wartości do 2 zmiennych
 
-X = 1, 2, 3 ; X[1] = 4
+X = 1, 2, 3 ; X[1] = 4 #Krotki nie można modyfikować
 
-X = [1, 2, 3] ; X[3] = 4
+X = [1, 2, 3] ; X[3] = 4 #Bład ponieważ lista ma 3 elementy a nie 4
 
-X = "abc" ; X.append("d")
+X = "abc" ; X.append("d") #Metoda append działa na listach a nie na String
 
-L = list(map(pow, range(8)))
+L = list(map(pow, range(8))) #Funkcja pow wymaga dwóch argumentów
