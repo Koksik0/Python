5a5669fe9faf66f9b98eba17deebc7ff08b89c55
Paweł Wacławik <pawelwoe@gmail.com>
2023-10-26 11:38:33 +0200

    Zadanie 3.5

--- Zestaw3/Zadanie5.py
+++ Zestaw3/Zadanie5.py
@@ -4,11 +4,15 @@ dots = '.' * gap
 parts = ['|' + dots] * x
 result = ''.join(parts) + '|'
 temp = ""
-for x in range(x+1):
-    temp += str(x)
-    if x%10 == 9 and str(x)[0]=='9':
-        temp += ' ' * (gap - len(str(x)))
+temp2 = 2
+for i in range(x+1):
+    temp += str(i)
+    if len(str(i)) + 1 > temp2:
+        temp += ' ' * (gap - len(str(i)))
+        temp2 += 1
     else:
-        temp += ' ' * (gap + (1 - len(str(x))))
+        temp += ' ' * (gap + (1 - len(str(i))))
+
+result += '\n'
+result += temp
 print(result)
-print(temp)
