28e20d1e2dad76a88b9e64746b879b52f96d425c
Paweł Wacławik <pawelwoe@gmail.com>
2023-10-31 18:34:01 +0100

    Zadanie 6.2

---
+++ Zestaw6/Zadanie2/points.py
@@ -0,0 +1,41 @@
+import math
+
+
+class Point:
+    """Klasa reprezentująca punkty na płaszczyźnie."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ")"
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def __eq__(self, other) -> bool:
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
+
+    def __ne__(self, other) -> bool:
+        return not self == other
+
+    def __add__(self, other) -> list:
+        return [self.x + other.x, self.y + other.y]
+
+    def __sub__(self, other) -> list:
+        return [self.x - other.x, self.y - other.y]
+
+    def __mul__(self, other) -> float:
+        return self.x * other.x + self.y * other.y
+
+    def cross(self, other) -> float:
+        return self.x * other.y - self.y * other.x
+
+    def length(self) -> float:
+        return math.sqrt(math.pow(self.x, 2) + math.pow(self.y, 2))
+
+    def __hash__(self):
+        return hash((self.x, self.y))
---
+++ Zestaw6/Zadanie2/test_points.py
@@ -0,0 +1,35 @@
+import math
+import unittest
+from points import Point
+
+
+class TestPoint(unittest.TestCase):
+    def setUp(self):
+        self.first_point = Point(1, 2)
+        self.second_point = Point(4, 7)
+
+    def test_eq_points(self):
+        self.assertEqual(self.first_point.__eq__(self.second_point), False)
+
+    def test_ne_ponts(self):
+        self.assertEqual(self.first_point.__ne__(self.second_point), True)
+
+    def test_add_points(self):
+        self.assertEqual(self.first_point.__add__(self.second_point), [5, 9])
+
+    def test_sub_points(self):
+        self.assertEqual(self.first_point.__sub__(self.second_point), [-3, -5])
+
+    def test_mul_points(self):
+        self.assertEqual(self.first_point.__mul__(self.second_point), 18)
+
+    def test_cross_points(self):
+        self.assertEqual(self.first_point.cross(self.second_point), -1)
+
+    def test_length_points(self):
+        self.assertEqual(self.first_point.length(), math.sqrt(5))
+        self.assertEqual(self.second_point.length(), math.sqrt(65))
+
+
+if __name__ == '__main__':
+    unittest.main()
